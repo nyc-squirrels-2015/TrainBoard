56c32ddc20f4581f51ae01c3239d0d0e34e019f5
Vivek Ratkalkar <vratkalkar@live.com>
2015-02-18 10:11:03 -0500

    Added login and sign up auth views and controller routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -2,10 +2,35 @@ get '/' do
   erb :welcome
 end
 
-get '/login' do
-  
+get '/signup' do
+  erb :"auth/signup"
 end
 
-get '/signup' do
-  
+post '/signup' do
+ @new_user = User.new(params[:user])
+ if @new_user.save
+  session[:user_id] = @new_user.id
+  redirect '/'
+ else
+   redirect '/signup'
+ end
+end
+
+get '/login' do
+  erb :"auth/login"
+end
+
+post '/login' do
+ @user = User.find_by(email: params[:user][:email])
+  if @user.try(:authenticate, params[:user][:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
